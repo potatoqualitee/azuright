5107d64cd36357c28ed8fa4256a46f181c386d9f
Chrissy LeMaire <clemaire@gmail.com>
2021-08-01 17:23:54 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,7 +2,10 @@ param (
    [string]$Directory = [System.IO.Path]::GetTempPath(),
    [int]$BlobPort = "10000",
    [int]$QueuePort = "10001",
-   [int]$TablePort = "10002"
+   [int]$TablePort = "10002",
+   [switch]$Silent,
+   [switch]$Loose,
+   [switch]$OAuth
 )
 
 if ($ismacos -or $islinux) {
@@ -21,4 +24,19 @@ npm install -g azurite
 Write-Output "Starting azurite"
 $null = New-Item -Type Directory -Force -Path $dir
 
-Start-Process -FilePath azurite -ArgumentList @("--silent", "--location", $dir, "--debug", $debuglog, "--blobPort", $BlobPort, "--queuePort", $QueuePort, "--tablePort", $TablePort)
+$params = @("--location", $dir, "--debug", $debuglog, "--blobPort", $BlobPort, "--queuePort", $QueuePort, "--tablePort", $TablePort)
+
+if ($Silent) {
+   $params += "--silent"
+}
+if ($Loose) {
+   $params += "--loose"
+}
+if ($OAuth) {
+   $params += "--oauth"
+}
+
+Start-Process -FilePath azurite -ArgumentList $params
+
+Write-Output "Default account name: devstoreaccount1`n"
+Write-Output "Default account key:`n Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
