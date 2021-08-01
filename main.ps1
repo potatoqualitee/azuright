a8c345349df8e3c061e11e035a8807cb39c46e42
Chrissy LeMaire <clemaire@gmail.com>
2021-08-01 16:29:22 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,20 +1,20 @@
 param (
-   [string[]]$Install,
+   [string]$Directory = [System.IO.Path]::GetTempPath(),
    [switch]$ShowLog
 )
 
 Write-Output "Installing azurite"
 npm install -g azurite
 
+Write-Output "Starting azurite"
 if ($ismacos -or $islinux) {
-   
+   $dir = "$Directory/azurite"
+   $null = New-Item -Type Directory -Force -Path $dir
+   azurite --silent --location $dir --debug "$dir/debug.log"
 }
 
 if ($iswindows) {
-   
+   $dir = "$Directory\azurite"
+   $null = New-Item -Type Directory -Force -Path 
+   azurite --silent --location $dir --debug "$dir\debug.log"
 }
-
-<#
-azurite --silent --location c:\azurite --debug c:\azurite\debug.log
-
-#>
