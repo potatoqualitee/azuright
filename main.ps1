d058bb220bba11c2163549ef15cf44f2b72f8092
Chrissy LeMaire <clemaire@gmail.com>
2021-08-01 23:33:18 +0200

    docker

--- main.ps1
+++ main.ps1
@@ -110,7 +110,8 @@ if ($CertPass) {
 if ($isLinux -or $isMacOS) {
    $null = Start-Process -FilePath azurite -ArgumentList $params -NoNewWindow -Verbose
 } else {
-   $null = Start-Job -FilePath C:\npm\prefix\azurite.ps1 -ArgumentList $params -Verbose
+   #$null = Start-Job -FilePath C:\npm\prefix\azurite.ps1 -ArgumentList $params -Verbose
+   docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite
 }
 
 Write-Verbose "
