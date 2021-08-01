739e7ec3ccda2daff2d46055c441f6f3f6bc533e
Chrissy LeMaire <clemaire@gmail.com>
2021-08-01 16:54:49 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,8 @@
 param (
    [string]$Directory = [System.IO.Path]::GetTempPath(),
-   [switch]$ShowLog
+   [int]$BlobPort = "10000",
+   [int]$QueuePort = "10001",
+   [int]$TablePort = "10002"
 )
 
 if ($ismacos -or $islinux) {
@@ -19,5 +21,4 @@ npm install -g azurite
 Write-Output "Starting azurite"
 $null = New-Item -Type Directory -Force -Path $dir
 
-Start-Process -FilePath azurite -ArgumentList @("--silent", "--location", $dir, "--debug", $debuglog)
-#azurite --silent --location $dir --debug $debuglog
+Start-Process -FilePath azurite -ArgumentList @("--silent", "--location", $dir, "--debug", $debuglog, "--blobPort", $BlobPort, "--queuePort", $QueuePort, "--tablePort", $TablePort)
