1b012c32efc6b2e3b244add22be51b98d70a32be
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-08-02 15:49:01 +0200

    update docs, reorder code

--- main.ps1
+++ main.ps1
@@ -31,13 +31,57 @@ if (-not $Directory) {
 
 $dir = Join-Path -Path $Directory -ChildPath azurite
 $debuglog = Join-Path -Path $dir -ChildPath debug.log
+$null = New-Item -Type Directory -Force -Path $dir
+
+if ($SelfSignedCert) {
+   Write-Verbose "Creating and trusting self-signed certificate"
+   $CertPath = Join-Path -Path $Directory -ChildPath cert.pem
+   $CertKeyPath = Join-Path -Path $Directory -ChildPath key.pem
+
+   if (-not $CertPass) {
+      $CertPass = "AzurIte365.Invoke"
+   }
+   
+   if ($isLinux) {
+      openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj '/CN=localhost' -keyout $CertKeyPath -out $CertPath -passout pass:$CertPass | Write-Verbose
+      $CertPass = $null
+      sudo cp $CertPath /etc/ssl/certs/ca.crt | Write-Verbose
+      sudo chmod 644 /etc/ssl/certs/ca.crt | Write-Verbose
+      sudo update-ca-certificates | Write-Verbose
+   }
+   if ($isMacOS) {
+      # It was super hard to register a self-signed certificate on a Mac, use mkcert to simplify
+      # they got it to work somehow
+      Write-Verbose "Installing mkcert"
+      $null = wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-darwin-amd64
+      $null = chmod +x mkcert-v1.4.3-darwin-amd64
+      $null = sudo mv ./mkcert-v1.4.3-darwin-amd64 /usr/local/bin/mkcert
+
+      Write-Verbose "Running mkcert"
+      mkcert -install | Write-Verbose
+      mkcert -key-file $CertKeyPath -cert-file $CertPath localhost | Write-Verbose
+   }
+   if ($isWindows) {
+      $PfxPath = Join-Path -Path $Directory -ChildPath cert.pfx
+      $cert = New-SelfSignedCertificate -DnsName localhost -CertStoreLocation "Cert:\CurrentUser\My" -KeyLength 2048 -KeyExportPolicy Exportable
+
+      # trust self signed cert
+      Write-Verbose "Trusting certificate"
+      certutil -addstore -f "ROOT" $CertPath | Write-Verbose
+
+      # azurite really didn't like the pfx, so we'll create and use a cert and key
+      $securepass = ConvertTo-SecureString -String $CertPass -AsPlainText -Force
+      $null = $cert | Export-PfxCertificate -FilePath $PfxPath -Password $securepass
+      openssl pkcs12 -in $PfxPath -nokeys -out $CertPath -passin pass:$CertPass | Write-Verbose
+      openssl pkcs12 -in $PfxPath -nocerts -out "$Directory\tempkey.pem" -nodes -passin pass:$CertPass | Write-Verbose
+      openssl rsa -in "$Directory\tempkey.pem" -out $CertKeyPath | Write-Verbose
+   }
+}
 
 Write-Verbose "Installing azurite"
 $null = npm install -g azurite
 
 Write-Verbose "Starting azurite"
-$null = New-Item -Type Directory -Force -Path $dir
-
 $params = @("--location", $dir, "--debug", $debuglog, "--blobPort", $BlobPort, "--queuePort", $QueuePort, "--tablePort", $TablePort)
 
 if ($Silent) {
@@ -51,50 +95,6 @@ if ($Loose) {
 if ($OAuth) {
    $params += "--oauth"
 }
-
-if ($SelfSignedCert) {
-   Write-Verbose "Creating and trusting self-signed certificate"
-   $PfxPath = Join-Path -Path $Directory -ChildPath cert.pfx
-   $CertPath = Join-Path -Path $Directory -ChildPath cert.pem
-   $CertKeyPath = Join-Path -Path $Directory -ChildPath key.pem
-
-   if (-not $CertPass) {
-      $CertPass = "AzurIte365.Invoke"
-   }
-   
-   if ($isLinux -or $isMacOs) {
-      Write-Verbose "Installing mkcert"
-      if ($isLinux) {
-         openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj '/CN=localhost' -keyout $CertKeyPath -out $CertPath -passout pass:$CertPass | Write-Verbose
-         $CertPass = $null
-         sudo cp $CertPath /etc/ssl/certs/ca.crt | Write-Verbose
-         sudo chmod 644 /etc/ssl/certs/ca.crt | Write-Verbose
-         sudo update-ca-certificates | Write-Verbose
-      }
-      if ($isMacOS) {
-         $null = wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-darwin-amd64
-         $null = chmod +x mkcert-v1.4.3-darwin-amd64
-         $null = sudo mv ./mkcert-v1.4.3-darwin-amd64 /usr/local/bin/mkcert
-      }
-      Write-Verbose "Running mkcert"
-      mkcert -install | Write-Verbose
-      mkcert -key-file $CertKeyPath -cert-file $CertPath localhost | Write-Verbose
-   } else {
-      $cert = New-SelfSignedCertificate -DnsName localhost -CertStoreLocation "Cert:\CurrentUser\My" -KeyLength 2048 -KeyExportPolicy Exportable
-
-      $securepass = ConvertTo-SecureString -String $CertPass -AsPlainText -Force
-      $null = $cert | Export-PfxCertificate -FilePath $PfxPath -Password $securepass
-
-      openssl pkcs12 -in $PfxPath -nokeys -out $CertPath -passin pass:$CertPass | Write-Verbose
-      openssl pkcs12 -in $PfxPath -nocerts -out "$Directory\tempkey.pem" -nodes -passin pass:$CertPass | Write-Verbose
-      openssl rsa -in "$Directory\tempkey.pem" -out $CertKeyPath | Write-Verbose
-
-      # trust self signed cert
-      Write-Verbose "Trusting certificate"
-      certutil -addstore -f "ROOT" $CertPath | Write-Verbose
-   }
-}
-
 if ($CertPath) {
    $proto = "https"
    $params += "--cert", $CertPath
