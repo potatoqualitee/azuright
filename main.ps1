c644e04e8c120d1ef2cbb446e9a842155afabaa3
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-08-02 15:03:12 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -64,7 +64,7 @@ if ($SelfSignedCert) {
 
    if ($isLinux -or $isMacOs) {
       if ($isLinux) {
-         sudo apt install libnss3-tools
+         choco install mkcert -y | Write-Verbose
       } else {
          brew install mkcert | Write-Verbose
          brew install nss | Write-Verbose
